8d6487f0bb34c6007fd3c7a0aa1692b16c99906d
Ye Kyaw Thu <wasedakuma@gmail.com>
2018-03-04 10:39:17 +0900

    Update play.sh

--- play.sh
+++ play.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# I used mpg123 player for my experiment.
+# And thus, you need to install mpg123 mp3 player in advanced
+#    https://www.mpg123.de/
+# actually, you can use any mp3 file player ...
+
 mpg123 $1;
